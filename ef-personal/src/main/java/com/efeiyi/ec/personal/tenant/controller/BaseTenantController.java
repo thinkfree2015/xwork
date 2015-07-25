dffddd80f651c1e118d1b9814ac0ffc3ff80b1f1
kaysonyang <kayson_yang@163.com>
2015-07-25 19:05:07 +0800

    增加解析三级域名

---
+++ ef-personal/src/main/java/com/efeiyi/ec/personal/tenant/controller/BaseTenantController.java
@@ -0,0 +1,46 @@
+package com.efeiyi.ec.personal.tenant.controller;
+
+import com.efeiyi.ec.tenant.model.Tenant;
+import com.ming800.core.base.controller.BaseController;
+import com.ming800.core.base.service.BaseManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.LinkedHashMap;
+
+
+/**
+ * Created by kayson on 2015/7/25.
+ *
+ */
+@Controller
+@RequestMapping("/basic")
+public class BaseTenantController extends BaseController {
+    @Autowired
+    BaseManager baseManager;
+    protected Tenant tenant;
+
+    public Tenant getTenant() {
+        return tenant;
+    }
+
+    public void setTenant(Tenant tenant) {
+        this.tenant = tenant;
+    }
+
+
+    @RequestMapping("/getTenant.do")
+    public Tenant getTenantfromDomain(HttpServletRequest request)throws Exception{
+        String subDommainName = (String)request.getAttribute("domainName");
+        if(!"master".equalsIgnoreCase(subDommainName)){
+            LinkedHashMap<String,Object> map = new LinkedHashMap<>();
+            String queryHql ="from Tenant t where t.name=:name";
+            map.put("name",subDommainName);
+            tenant =(Tenant) baseManager.getUniqueObjectByConditions(queryHql,map);
+        }
+        System.out.println(tenant.getName()+tenant.getContent());
+      return tenant;
+    }
+}
