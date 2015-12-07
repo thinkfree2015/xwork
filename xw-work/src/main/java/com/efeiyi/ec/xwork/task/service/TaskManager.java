d222e7ed8dca21e0247b9be7a6849f1035ba51fc
kaysonyang <kayson_yang@163.com>
2015-12-07 10:14:32 +0800

    增加 task service 类

--- xw-work/src/main/java/com/efeiyi/ec/xwork/task/service/TaskManager.java
+++ xw-work/src/main/java/com/efeiyi/ec/xwork/task/service/TaskManager.java
@@ -1,8 +1,13 @@
 package com.efeiyi.ec.xwork.task.service;
 
+import com.efeiyi.ec.xw.task.model.Task;
+
+import java.util.Map;
+
 /**
  * Created by Administrator on 2015/12/7.
  *
  */
 public interface TaskManager {
+    Task createTask(Map map)throws Exception;
 }
--- xw-work/src/main/java/com/efeiyi/ec/xwork/task/service/impl/TaskManagerImpl.java
+++ xw-work/src/main/java/com/efeiyi/ec/xwork/task/service/impl/TaskManagerImpl.java
@@ -1,7 +1,28 @@
 package com.efeiyi.ec.xwork.task.service.impl;
 
+import com.efeiyi.ec.xw.task.model.Task;
+import com.efeiyi.ec.xwork.task.service.TaskManager;
+import com.ming800.core.base.dao.XdoDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
 /**
  * Created by Administrator on 2015/12/7.
+ *
  */
-public class TaskManagerImpl {
+@Service
+public class TaskManagerImpl implements TaskManager {
+    @Autowired
+    private XdoDao xdoDao;
+    @Override
+    public Task createTask(Map map) throws Exception {
+        Task task = new Task();
+        if (map!=null && !map.isEmpty()){
+
+        }
+        xdoDao.saveOrUpdateObject(Task.class.getName(),task);
+        return task;
+    }
 }
