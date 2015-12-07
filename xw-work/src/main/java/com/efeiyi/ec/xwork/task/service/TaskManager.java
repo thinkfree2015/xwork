989c4d281ef255e60ddf067ae3f2a7399e92bd7f
kaysonyang <kayson_yang@163.com>
2015-12-07 09:39:02 +0800

    增加 task service 类

---
+++ xw-work/src/main/java/com/efeiyi/ec/xwork/task/service/TaskManager.java
@@ -0,0 +1,8 @@
+package com.efeiyi.ec.xwork.task.service;
+
+/**
+ * Created by Administrator on 2015/12/7.
+ *
+ */
+public interface TaskManager {
+}
---
+++ xw-work/src/main/java/com/efeiyi/ec/xwork/task/service/impl/TaskManagerImpl.java
@@ -0,0 +1,7 @@
+package com.efeiyi.ec.xwork.task.service.impl;
+
+/**
+ * Created by Administrator on 2015/12/7.
+ */
+public class TaskManagerImpl {
+}
